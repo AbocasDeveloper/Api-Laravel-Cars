fe40c19e4a2b399f118c3f343d84f10f0456addc
AbocasDeveloper <abocasdeveloper@gmail.com>
2021-10-04 20:32:25 +0200

    Guardar y validar coches

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Helpers\JwtAuth;
+use App\Car;
 
 class CarController extends Controller
 {
@@ -22,4 +23,62 @@ class CarController extends Controller
             echo "NO AUTENTICADO EN INDEX DE CARCONTROLLER..."; die();
         }
     }
-}
+
+    //Método que nos guarda un coche, recibiendo los datos anteriormente
+    public function store(Request $request){
+        //Autenticación
+        $hash = $request->header('Authorization', null);
+
+        $jwtAuth = new JwtAuth();
+
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if($checkToken){
+            //Recoger datos por POST
+            $json = $request->input('json', null);
+            $params = json_decode($json);
+            $params_array = json_decode($json, true); //Al escribir true, nos devuelve un array
+
+            //Conseguir el usuario identificado
+            $user = $jwtAuth->checkToken($hash, true); 
+            
+            //Validamos los datos que hemos recibido por POST
+            $validate = \Validator::make($params_array, [
+                'title' => 'required',
+                'description' => 'required',
+                'price' => 'required',
+                'status' => 'required'
+            ]);
+
+            //Vemos si tenemos errores en la validacion
+            if($validate->fails()){
+                return response()->json($validate->errors(), 400);
+            }
+
+            //Guardar el coche
+            $car = new Car();
+            $car->user_id = $user->sub;
+            $car->title = $params->title;
+            $car->description = $params->description;
+            $car->price = $params->price;
+            $car->status = $params->status;
+
+            $car->save();
+
+            $data = array(
+                'car' => $car,
+                'status' => 'success',
+                'code' => 200
+            );
+        }
+        else{
+            //Devolvemos un error
+            $data = array(
+                'message' => 'Login incorrecto!',
+                'status' => 'error',
+                'code' => 400
+            );
+        }
+        return response()->json($data, 200);
+    }
+} //Final de la clase
