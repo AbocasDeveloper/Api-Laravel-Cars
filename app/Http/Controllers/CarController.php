6bc5da502d660fd3c8d8f99f0db739cd15193f45
AbocasDeveloper <abocasdeveloper@gmail.com>
2021-10-13 22:54:36 +0200

    CRUD Cars - API Finalizada

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -8,23 +8,28 @@ use App\Car;
 
 class CarController extends Controller
 {
+    //Metodo que nos devuelve los coches que se encuentran en la BBDD
     public function index(Request $request){
 
-        $hash = $request->header('Authorization', null);
+        $cars = Car::all()->load('user');
 
-        $jwtAuth = new JwtAuth();
-
-        $checkToken = $jwtAuth->checkToken($hash);
-
-        if($checkToken){
-            echo "INDEX DE CARCONTROLLER AUTENTICADO"; die();
-        }
-        else{
-            echo "NO AUTENTICADO EN INDEX DE CARCONTROLLER..."; die();
-        }
+        return response()->json(array(
+            'cars' => $cars,
+            'status' => 'success'
+        ), 200);
     }
 
-    //Método que nos guarda un coche, recibiendo los datos anteriormente
+    //Metodo que nos muestra los detalles de un coche, pasando el ID de este
+    public function show($id){
+        $car = Car::find($id)->load('user');
+
+        return response()->json(array(
+            'car' => $car,
+            'status' => 'success'
+        ), 200);
+    }
+
+    //Metodo que nos guarda un coche, recibiendo los datos anteriormente
     public function store(Request $request){
         //Autenticación
         $hash = $request->header('Authorization', null);
@@ -81,4 +86,88 @@ class CarController extends Controller
         }
         return response()->json($data, 200);
     }
+
+    //Metodo que nos permite editar un coche
+    public function update($id, Request $request){
+        //Autenticación
+        $hash = $request->header('Authorization', null);
+
+        $jwtAuth = new JwtAuth();
+
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if($checkToken){
+            //Recoger datos por POST
+            $json = $request->input('json', null);
+            $params = json_decode($json);
+            $params_array = json_decode($json, true); //Al escribir true, nos devuelve un array
+            
+            //Validamos los datos que hemos recibido por POST
+            $validate = \Validator::make($params_array, [
+                'title' => 'required',
+                'description' => 'required',
+                'price' => 'required',
+                'status' => 'required'
+            ]);
+
+            //Vemos si tenemos errores en la validacion
+            if($validate->fails()){
+                return response()->json($validate->errors(), 400);
+            }
+
+            //Actualizamos el coche
+            $car = Car::where('id', $id)->update($params_array);
+
+            $data = array(
+                'car' => $params,
+                'status' => 'success',
+                'code' => 200
+            );
+        }
+        else{
+            //Devolvemos un error
+            $data = array(
+                'message' => 'Login incorrecto!',
+                'status' => 'error',
+                'code' => 400
+            );
+        }
+        return response()->json($data, 200);
+    }
+
+    //Metodo que nos elimina un coche
+    public function destroy($id, Request $request){
+        //Autenticación
+        $hash = $request->header('Authorization', null);
+
+        $jwtAuth = new JwtAuth();
+
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if($checkToken){
+            //Comprobamos que existe el registro
+            $car = Car::find($id);
+
+            //Eliminamos el coche
+            $car->delete();
+
+            $data = array(
+                'car' => $car,
+                'status' => 'success',
+                'code' => 200
+            );
+        }
+        else
+        {
+            //Devolvemos un error
+            $data = array(
+                'message' => 'Login incorrecto!',
+                'status' => 'error',
+                'code' => 400
+            );
+        }
+
+        return response()->json($data, 200);
+    }
+
 } //Final de la clase
